a070d85faa9dd097c7994c45290c8d2eef75acd3
CIBI.P _GCEB <brohari740@gmail.com>
2022-02-16 19:02:23 +0530

    updated the making.html, making.css, menu.js

--- Html/menu.js
+++ Html/menu.js
@@ -1,11 +1,22 @@
 function menufunction()
 {
-    
-    console.log("hello");
     var x=document.getElementById("menu-menu-js");
     if (x.style.display != "flex")
     {
         x.style.display = "flex";
+    }
+    else{
+        x.style.display = "none";
+    }
+}
+
+function opencexpla()
+{
+    console.log("hello");
+    var x=document.getElementById("opencexpla");
+    if (x.style.display != "block")
+    {
+        x.style.display = "block";
     }
     else{
         x.style.display = "none";
